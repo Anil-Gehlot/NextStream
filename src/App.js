1e1ebda80f1082151587cad6ec7681b41d057410
anil-gehlot <anilgehlot2000@gmail.com>
2024-09-18 22:05:05 +0530

    Containig Routers and Redux Store Provider.

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { Provider } from "react-redux";
+import "./App.css";
+import Body from "./components/Body";
+import store from "./utils/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+import Result from "./components/Result";
+import Error from "./components/Error";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    errorElement : <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+      {
+        path: "result",
+        element: <Result />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <RouterProvider router={appRouter} />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
