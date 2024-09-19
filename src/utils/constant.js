41bf6d65002b36a1a7e4b04fa31183da520106a3
anil-gehlot <anilgehlot2000@gmail.com>
2024-09-19 09:32:32 +0530

    contains all the API's and constant data.

---
+++ src/utils/constant.js
@@ -0,0 +1,167 @@
+const YOUTUBE_API_KEY = "AIzaSyCtWHumXM_UBTAzL_fgWxB-7_huvkARmLc";
+
+// original 
+// const YOUTUBE_API_KEY = "AIzaSyDAEQJKO5QQXnlViaIY9zEJzT41WbF2_VY"
+
+
+export const YOUTUBE_VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  YOUTUBE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const COMMENTS_API =
+  "https://www.googleapis.com/youtube/v3/commentThreads?textFormat=plainText&part=snippet&maxResults=50&key=" +
+  YOUTUBE_API_KEY +
+  "&videoId=";
+
+export const VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=" +
+  YOUTUBE_API_KEY +
+  "&id=";
+
+export const SEARCH_RESULT_API =
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=" +
+  YOUTUBE_API_KEY +
+  "&q=";
+
+export function formatViews(views) {
+  if (views >= 1000000) {
+    return (views / 1000000).toFixed(1) + "M"; // for million
+  } else if (views >= 1000) {
+    return (views / 1000).toFixed(1) + "K"; // for thousand
+  }
+  return views; // return the original value if less than 1000
+}
+
+export const OFFSET_LIVE_CHAT = 25;
+
+export const commentsData = [
+  {
+    name: "anil",
+    Comment: "a niof f knfioasv v snv ",
+    replies: [
+      {
+        name: "anil",
+        Comment: "a niof f knfioasv v snv ",
+        replies: [],
+      },
+      {
+        name: "anil",
+        Comment: "a niof f knfioasv v snv ",
+        replies: [],
+      },
+      {
+        name: "anil",
+        Comment: "a niof f knfioasv v snv ",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "anil",
+    Comment: "a niof f knfioasv v snv ",
+    replies: [
+      {
+        name: "anil",
+        Comment: "a niof f knfioasv v snv ",
+        replies: [],
+      },
+      {
+        name: "anil",
+        Comment: "a niof f knfioasv v snv ",
+        replies: [
+          {
+            name: "anil",
+            Comment: "a niof f knfioasv v snv ",
+            replies: [],
+          },
+          {
+            name: "anil",
+            Comment: "a niof f knfioasv v snv ",
+            replies: [],
+          },
+          {
+            name: "anil",
+            Comment: "a niof f knfioasv v snv ",
+            replies: [],
+          },
+        ],
+      },
+      {
+        name: "anil",
+        Comment: "a niof f knfioasv v snv ",
+        replies: [
+          {
+            name: "anil",
+            Comment: "a niof f knfioasv v snv ",
+            replies: [],
+          },
+          {
+            name: "anil",
+            Comment: "a niof f knfioasv v snv ",
+            replies: [],
+          },
+          {
+            name: "anil",
+            Comment: "a niof f knfioasv v snv ",
+            replies: [
+              {
+                name: "anil",
+                Comment: "a niof f knfioasv v snv ",
+                replies: [],
+              },
+              {
+                name: "anil",
+                Comment: "a niof f knfioasv v snv ",
+                replies: [],
+              },
+              {
+                name: "anil",
+                Comment: "a niof f knfioasv v snv ",
+                replies: [],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "anil",
+    Comment: "a niof f knfioasv v snv ",
+    replies: [
+      {
+        name: "anil",
+        Comment: "a niof f knfioasv v snv ",
+        replies: [],
+      },
+      {
+        name: "anil",
+        Comment: "a niof f knfioasv v snv ",
+        replies: [],
+      },
+      {
+        name: "anil",
+        Comment: "a niof f knfioasv v snv ",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "anil",
+    Comment: "a niof f knfioasv v snv ",
+    replies: [],
+  },
+  {
+    name: "anil",
+    Comment: "a niof f knfioasv v snv ",
+    replies: [],
+  },
+  {
+    name: "anil",
+    Comment: "a niof f knfioasv v snv ",
+    replies: [],
+  },
+];
