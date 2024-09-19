ea48cb5bbf740305d25b89e6ca9df06476ca2d14
anil-gehlot <anilgehlot2000@gmail.com>
2024-09-19 15:02:46 +0530

    contains all the API's and constant data.

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -6,7 +6,7 @@ export const YOUTUBE_VIDEO_API =
   YOUTUBE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const COMMENTS_API =
   "https://www.googleapis.com/youtube/v3/commentThreads?textFormat=plainText&part=snippet&maxResults=50&key=" +
